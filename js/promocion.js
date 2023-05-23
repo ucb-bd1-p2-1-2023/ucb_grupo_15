1ea4ec349c82a0cd18ae1e646d361e5167b6f83d
lPatrickll <patrickgonzalesquintana@gmail.com>
2023-05-23 18:31:07 -0300

    Ordene y añadi el historial

---
+++ js/promocion.js
@@ -0,0 +1,8 @@
+function submitPromocion(){
+    var clave = document.getElementById("id_clave");
+    var descuento = document.getElementById("id_descuento");
+    var fecha_ini = document.getElementById("id_date_inicio");
+    var fecha_fin = document.getElementById("id_date_fin");
+
+    alert('Hola');
+}
